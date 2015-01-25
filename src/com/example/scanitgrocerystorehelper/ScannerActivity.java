dd943d43bcf895eacbde3a6d01fdd2954c3ebd6d
Jacob Oakes <oakesja@rose-hulman.edu>
2015-01-24 19:34:24 -0500

    added barcode scanning by integrating with zxing barcode scanner

--- src/com/example/scanitgrocerystorehelper/ScannerActivity.java
+++ src/com/example/scanitgrocerystorehelper/ScannerActivity.java
@@ -1,6 +1,11 @@
 package com.example.scanitgrocerystorehelper;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ScannerActivity extends DrawerActivity {
 
@@ -9,5 +14,18 @@ public class ScannerActivity extends DrawerActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scanner);
 		getActionBar().setTitle(R.string.title_activity_scanner);
+		IntentIntegrator it = new IntentIntegrator(this);
+		it.initiateScan();
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(
+				requestCode, resultCode, data);
+		if (scanResult != null) {
+			Toast.makeText(this, scanResult.toString(), Toast.LENGTH_LONG).show();;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
